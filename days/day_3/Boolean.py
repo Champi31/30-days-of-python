75699d78428415899727d0229fac98473b2dcbce
Champi31 <r3r3eydeitaca@gmail.com>
2023-01-20 11:13:14 +0100

    operators

---
+++ days/day_3/Boolean.py
@@ -0,0 +1,11 @@
+age = 16
+int_number = 1.76
+#convert int to float
+float_number = float(int_number)
+print (float_number)
+
+(complex (8,3))
+base = input("TU, gilipollas, dime un puto numero ya para la puta base del puto triangulo, o sino te rajo ")
+height = input("Y ahora dime una puta altura del triangulo gilipollas  ")
+Area_del_triangulo = (base * height*0.5)
+print (Area_del_triangulo)
