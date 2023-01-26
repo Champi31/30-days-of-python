7c96ea6ef968a1bc70e7f958950192fedd182abb
Champi31 <r3r3eydeitaca@gmail.com>
2023-01-26 09:11:31 +0100

    ayudenme

--- days/day_3/Boolean.py
+++ days/day_3/Boolean.py
@@ -5,7 +5,21 @@ float_number = float(int_number)
 print (float_number)
 
 (complex (8,3))
-base = input("TU, gilipollas, dime un puto numero ya para la puta base del puto triangulo, o sino te rajo ")
-height = input("Y ahora dime una puta altura del triangulo gilipollas  ")
-Area_del_triangulo = (base * height*0.5)
+
+base = int(input("TU, gilipollas, dime un puto numero ya para la puta base del puto triangulo, o sino te rajo"))
+height = int(input("Y ahora dime una puta altura del triangulo gilipollas" ))
+Area_del_triangulo = 0.5*base*height
 print (Area_del_triangulo)
+
+a = int(input("Dame el puto lado a"))
+b = int(input("Dame el puto lado b"))
+c = int(input("Dame el puto lado c"))
+perimeter = a+b+c
+print (perimeter)
+
+lenght = int(input("Gilipollas, o me das el largo del triangulo o tu de aqui no pasas"))
+width = int(input("*le saca una navaja* dame todo el ancho que tengas!"))
+area_del_triangulo = lenght*width
+print(area_del_triangulo)
+
+
