39701119e37d8c346cf2d6fe741ba608884ab7f3
Champi31 <r3r3eydeitaca@gmail.com>
2023-01-27 11:14:25 +0100

    odio mates

--- days/day_3/Boolean.py
+++ days/day_3/Boolean.py
@@ -6,20 +6,50 @@ print (float_number)
 
 (complex (8,3))
 
-base = int(input("TU, gilipollas, dime un puto numero ya para la puta base del puto triangulo, o sino te rajo"))
-height = int(input("Y ahora dime una puta altura del triangulo gilipollas" ))
+base = int(input("Dame la base del triangulo"))
+height = int(input("Dame la altura del triangulo"))
 Area_del_triangulo = 0.5*base*height
 print (Area_del_triangulo)
 
-a = int(input("Dame el puto lado a"))
-b = int(input("Dame el puto lado b"))
-c = int(input("Dame el puto lado c"))
+a = int(input("Dame el lado a"))
+b = int(input("Dame el lado b"))
+c = int(input("Dame el lado c"))
 perimeter = a+b+c
 print (perimeter)
 
-lenght = int(input("Gilipollas, o me das el largo del triangulo o tu de aqui no pasas"))
-width = int(input("*le saca una navaja* dame todo el ancho que tengas!"))
-area_del_triangulo = lenght*width
-print(area_del_triangulo)
+lenght = int(input("Dame el largo del rectangulo"))
+width = int(input("Dame el ancho del rectangulo"))
+area_del_rectangulo = lenght*width
+perimeter_del_rectangulo = 2*(lenght+width)
+print(area_del_rectangulo)
+print(perimeter_del_rectangulo)
 
+radius = int(input("Dame el radio del triangulo"))
+pi = 3.14
+area_del_circulo = pi*radius**2
+print (area_del_circulo)
+circunferencia = 2*pi*radius
+print (circunferencia)
 
+x = int(input("Dame el valor de la x"))
+m = 2
+y = m*x - 2
+print (y)
+
+x = 2
+y = 2
+m = (y*2 - y*1)/(x*2 - x*1)
+print(m)
+
+x = 6
+y = 10
+m = (y*2 - y*1)/(x*2 - x*1)
+print(m)
+
+import math 
+x_1 = 2
+x_2 = 6
+y_1 = 2
+y_2 = 10
+d = math.sqrt(((x_2 - x_1)**2 + (y_2 - y_1)**2))
+print (d)
