b316f57536710a58ee536cc69b9cb88ca65148c6
Champi31 <r3r3eydeitaca@gmail.com>
2023-02-10 11:08:21 +0100

    patata

--- days/day_3/Boolean.py
+++ days/day_3/Boolean.py
@@ -1,51 +1,52 @@
+# 1 Declare your age as integer variable
 age = 16
 int_number = 1.76
 #convert int to float
 float_number = float(int_number)
 print (float_number)
-
+# 2 Declare your height as a float variable
 (complex (8,3))
-
+# 3 Declare a variable that store a complex number
 base = int(input("Dame la base del triangulo"))
 height = int(input("Dame la altura del triangulo"))
 Area_del_triangulo = 0.5*base*height
 print (Area_del_triangulo)
-
+# 4 Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
 a = int(input("Dame el lado a"))
 b = int(input("Dame el lado b"))
 c = int(input("Dame el lado c"))
 perimeter = a+b+c
 print (perimeter)
-
-lenght = int(input("Dame el largo del rectangulo"))
+# 5 Write a script that promps the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c)
+ lenght = int(input("Dame el largo del rectangulo"))
 width = int(input("Dame el ancho del rectangulo"))
 area_del_rectangulo = lenght*width
 perimeter_del_rectangulo = 2*(lenght+width)
 print(area_del_rectangulo)
 print(perimeter_del_rectangulo)
-
+# 6 Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
 radius = int(input("Dame el radio del triangulo"))
 pi = 3.14
 area_del_circulo = pi*radius**2
 print (area_del_circulo)
 circunferencia = 2*pi*radius
 print (circunferencia)
-
+# 7 Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
 x = int(input("Dame el valor de la x"))
 m = 2
 y = m*x - 2
 print (y)
-
+# 8 Calculate the slope, x-intercept and y-intercept of y = 2x -2
 x = 2
 y = 2
 m = (y*2 - y*1)/(x*2 - x*1)
 print(m)
-
+# 9 Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
 x = 6
 y = 10
 m = (y*2 - y*1)/(x*2 - x*1)
 print(m)
-
+# 10 Compare the slopes in tasks 8 and 9.
 import math 
 x_1 = 2
 x_2 = 6
@@ -53,3 +54,14 @@ y_1 = 2
 y_2 = 10
 d = math.sqrt(((x_2 - x_1)**2 + (y_2 - y_1)**2))
 print (d)
+# 11 Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+print (m==d)
+x_3 = -3
+y = x_3**2 + 6*x_3 + 9
+print (y==0)
+# 12 Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+pyt_len = len('python')
+dra_len = len('dragon')
+print (pyt_len)
+print (dra_len)
+# 13 Use and operator to check if 'on' is found in both 'python' and 'dragon'
