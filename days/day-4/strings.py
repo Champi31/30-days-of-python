94d051ae02e48394280fa8ddd9f0831b7f0d9c72
Champi31 <r3r3eydeitaca@gmail.com>
2023-02-16 09:13:05 +0100

    lechuga

---
+++ days/day-4/strings.py
@@ -0,0 +1,8 @@
+# 1 Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+T = 'Thirty'
+D = 'Days'
+O = 'Of'
+P = 'Python'
+space = ' '
+full_string = T + space + D + space + O + space + P
+print (full_string)
