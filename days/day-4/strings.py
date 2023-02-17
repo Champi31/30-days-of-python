62bb2cef3c6ed94a2ba093327bdeec810cfb9acf
Champi31 <r3r3eydeitaca@gmail.com>
2023-02-17 11:11:43 +0100

    cebolla_vieja

--- days/day-4/strings.py
+++ days/day-4/strings.py
@@ -6,3 +6,32 @@ P = 'Python'
 space = ' '
 full_string = T + space + D + space + O + space + P
 print (full_string)
+# 2 Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+C = 'Coding'
+F = 'For'
+A = 'All'
+Space = ' '
+full_string2 = C + Space + F + Space + A
+print ( full_string2)
+# 3 Declare a variable named company and assign it to an initial value "Coding For All".
+company = "Coding For All"
+print (company)
+print (len(company))
+mayus = company.upper()
+print (mayus)
+minum = company.lower()
+print (minum)
+cap = company.capitalize()
+tit = company.title()
+swp = company.swapcase()
+print (cap)
+print (tit)
+print (swp)
+cut = slice(0, 6)
+print (company[cut])
+Cd = 'Coding'
+print (company.index(Cd))
+print (company.rindex(Cd))
+print (company.rfind(Cd))
+print (company.find(Cd))
+print (company.replace('Coding', 'Python'))
