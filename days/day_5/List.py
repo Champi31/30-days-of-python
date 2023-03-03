eef78ee0285907d3298e7613809ea3e20661acb9
Champi31 <r3r3eydeitaca@gmail.com>
2023-03-03 11:14:17 +0100

    pimiento

--- days/day_5/List.py
+++ days/day_5/List.py
@@ -1,4 +1,18 @@
 print ([])
-compra = ['patatas', 'avichueltas', 'cebolla', 'pepino', 'brocoli', 'lechuga']
+compra = ['patatas', 'avichueltas', 'cebolla', 'pepino', 'brocoli', 'lechuga', 'garbanzo']
 print (compra)
 print (len(compra))
+print (compra[0])
+print (compra[3])
+print (compra[-1])
+mixed_data_types = ['Ulises', '16', '1.72', 'single', 'Jerez']
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print (it_companies)
+print (len(it_companies))
+print (it_companies[0])
+print (it_companies[3])
+print (it_companies[-1])
+it_companies[3] = 'Android'
+print (it_companies)
+it_companies.insert(7, 'IT')
+print (it_companies)
